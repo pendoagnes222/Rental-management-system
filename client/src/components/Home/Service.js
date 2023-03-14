37f4fda1810bd2cd5d23a952becb77078fa57064
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-03-14 20:25:26 +0300

    airbnb

--- client/src/components/Home/Service.js
+++ client/src/components/Home/Service.js
@@ -1,33 +1,77 @@
 import React from 'react'
+import Card from './Card'
 import './Service.css'
 
-import { FaWifi, FaSwimmingPool, FaLaptop, FaKey} from 'react-icons/fa';
 
 
 
-
-function Service() {
+function Services() {
   return (
-    <div className='services__info'>
-      <h1 >Popular amenities in nairobi</h1>
-      <div className='icons'>
-      <FaWifi />
-      < FaSwimmingPool />
-      <FaLaptop />
-      <FaKey />
+    <div className='amenities__info'>
+      <h1>Serviced apartments in Nairobi</h1>
+        <div className='cardi'>
+        <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVooZK0-yXD07UrYjgMxDwLaUBOvOuu9Z-Xw&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+           />
 
-
-
-
-
+         <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFCjZpC0qDyk3L4kmR-Xn0GMF1EsFJTNAogA&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+           />
+           <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaUa9Gqf9gwZUgGN0LtRjs5fS-3K2juZLC6Q&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+           />
             
-               
-     
-      </div>
-       
+            
+  
+        </div>
+        <div className='cardi'>
+          
+        <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzJIIUWE8Ryl2xc1orgCdkW8stzCZbmysWng&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+           />
+       <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFCjZpC0qDyk3L4kmR-Xn0GMF1EsFJTNAogA&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+             />
+      <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+           />
+          
+        </div>
+        <div className='cardi'>
+          
+         <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMdHOwbwhGbbVBbqRR3Wv8Zt84EJEnJ015Ww&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+           />
+         <Card  
+            
+           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
+           title="serviced apartment in Nairobi"
+           description="Superhost with a stunning view of the beachside "
+          />
+     <Card  
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
+            title="serviced apartment in Nairobi"
+            description="Superhost with a stunning view of the beachside "
+            />
 
+ </div>
+        <h2>center</h2>
     </div>
   )
 }
 
-export default Service
+export default Services
