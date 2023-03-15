61f6835729af714eb865ae5478e62567ede1134c
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-03-15 23:48:23 +0300

    airbnb

--- client/src/components/Home/Amenities.js
+++ client/src/components/Home/Amenities.js
@@ -9,15 +9,15 @@ function Amenities() {
       <h1 >Popular amenities in nairobi</h1>
       <div className='amenities_img'>
       <Card src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAZLXB304ZvIIb9O7MO6FGICymaO8iG9qQblLg3HZozwLZVu2uXm2d1lkpgQz_PL54CXc&usqp=CAU"
-            title="serviced apartment in Nairobi"
+            title="Internet Wifi connection"
             />
 
       <Card src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReS0iCmXq09cy1Mo3erYgxnMLycwzUKkEbtQ&usqp=CAU"
-            title="serviced apartment in Nairobi"
+            title="Open air swimming pool"
             /> 
 
       <Card src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0uVkCFZtlxfAOHOG_2TTh9-oaR8bQmcdNdg&usqp=CAU"
-            title="serviced apartment in Nairobi"
+            title="Fitness Center"
             />      
       </div>
 
--- client/src/components/Home/Service.js
+++ client/src/components/Home/Service.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-/*import React from 'react'
-import './Amenities.css'
-import Card from './Card'*/
-
-
-/*function Amenities() {
-  return (
-    <div className='amenities__info'>
-      <h1 >Popular amenities in nairobi</h1>
-      <div className='amenities_img'>
-      <Card src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAZLXB304ZvIIb9O7MO6FGICymaO8iG9qQblLg3HZozwLZVu2uXm2d1lkpgQz_PL54CXc&usqp=CAU"
-            title="serviced apartment in Nairobi"
-            />
-
-      <Card src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReS0iCmXq09cy1Mo3erYgxnMLycwzUKkEbtQ&usqp=CAU"
-            title="serviced apartment in Nairobi"
-            /> 
-
-      <Card src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0uVkCFZtlxfAOHOG_2TTh9-oaR8bQmcdNdg&usqp=CAU"
-            title="serviced apartment in Nairobi"
-            />      
-      </div>
-=======
 import React from 'react'
 import Card from './Card'
 import './Service.css'
@@ -37,18 +13,18 @@ function Services() {
         <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVooZK0-yXD07UrYjgMxDwLaUBOvOuu9Z-Xw&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            description="Lavish 3bedroom in South C "
            />
 
          <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFCjZpC0qDyk3L4kmR-Xn0GMF1EsFJTNAogA&usqp=CAU"
-            title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            title="Cosy 1Bedroom in Kileleshwa"
+            description="Cozy 1Bedroom flat in Pangani "
            />
            <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaUa9Gqf9gwZUgGN0LtRjs5fS-3K2juZLC6Q&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            description="Lavish 4Bedroom in Lavignton"
            />
             
             
@@ -59,17 +35,17 @@ function Services() {
         <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzJIIUWE8Ryl2xc1orgCdkW8stzCZbmysWng&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            description="Luxury Studio apartment in Kilimani"
            />
        <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFCjZpC0qDyk3L4kmR-Xn0GMF1EsFJTNAogA&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            description=" 1 Bedroom flat in Westlands"
              />
       <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            description="Cozy 2Bedroom apartment in Savannah "
            />
           
         </div>
@@ -78,29 +54,26 @@ function Services() {
          <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMdHOwbwhGbbVBbqRR3Wv8Zt84EJEnJ015Ww&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            description="Stunning 3Bedroom in Westlands "
            />
          <Card  
             
            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
            title="serviced apartment in Nairobi"
-           description="Superhost with a stunning view of the beachside "
+           description="2Bedroom Fully furnished in Riverside "
           />
      <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Superhost with a stunning view of the beachside "
+            description="4Bedroom Apartment in Westlands"
             />
->>>>>>> 37f4fda1810bd2cd5d23a952becb77078fa57064
+
 
  </div>
-        <h2>center</h2>
+      
     </div>
+    
   )
 }
 
-<<<<<<< HEAD
-export default Amenities*/
-=======
 export default Services
->>>>>>> 37f4fda1810bd2cd5d23a952becb77078fa57064
