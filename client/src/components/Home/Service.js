bbff0b50ffe6f56cf9509f988d309d14db08d0b6
pendoagnes222 <pendoagnes22@gmail.com>
2023-03-16 11:38:06 +0300

    airbnb

--- client/src/components/Home/Service.js
+++ client/src/components/Home/Service.js
@@ -13,18 +13,18 @@ function Services() {
         <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVooZK0-yXD07UrYjgMxDwLaUBOvOuu9Z-Xw&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Lavish 3bedroom in South C "
+            description="Lavish 3 Bedroom in South C "
            />
 
          <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFCjZpC0qDyk3L4kmR-Xn0GMF1EsFJTNAogA&usqp=CAU"
             title="Cosy 1Bedroom in Kileleshwa"
-            description="Cozy 1Bedroom flat in Pangani "
+            description="Cozy 1 Bedroom flat in Pangani "
            />
            <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaUa9Gqf9gwZUgGN0LtRjs5fS-3K2juZLC6Q&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Lavish 4Bedroom in Lavignton"
+            description="Lavish 4 Bedroom in Lavignton"
            />
             
             
@@ -45,7 +45,7 @@ function Services() {
       <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Cozy 2Bedroom apartment in Savannah "
+            description="Cozy 2 Bedroom apartment in Savannah "
            />
           
         </div>
@@ -54,18 +54,18 @@ function Services() {
          <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMdHOwbwhGbbVBbqRR3Wv8Zt84EJEnJ015Ww&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="Stunning 3Bedroom in Westlands "
+            description="Stunning 3 Bedroom in Westlands "
            />
          <Card  
             
            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
            title="serviced apartment in Nairobi"
-           description="2Bedroom Fully furnished in Riverside "
+           description="2 Bedroom Fully furnished in Riverside "
           />
      <Card  
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHdi_ssWLxJ_kC6HO_qxRxv_i-BX-TzSiB7A&usqp=CAU"
             title="serviced apartment in Nairobi"
-            description="4Bedroom Apartment in Westlands"
+            description="4 Bedroom Apartment in Westlands"
             />
 
 
