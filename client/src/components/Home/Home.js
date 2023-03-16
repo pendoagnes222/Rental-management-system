ef90c48fd57861cf190f842b02da01a564404516
pendoagnes222 <pendoagnes22@gmail.com>
2023-03-16 12:15:32 +0300

    airbnb

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -22,14 +22,14 @@ function Home() {
              <Card 
              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsM11SW3DymOkDNUCj7_GEewdu0DYTssrQDw&usqp=CAU"
             title="Condo in Nairobi"
-            description="Unique activities we can do together, led by a world of hosts."
+            description="Well-appointed apartments with hotel-lik amenities"
             viewImagesUrl="https:bn:ANd9GcSUlhuKexoWBekJRxsVR4QHkrWJnCpqrw9iGXmJ_y48fis3h8s6Y24x_OTaba2hfGkQ8T0&usqp=CAU"
             />
             
             <Card 
              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwdLQQoUu7kjYqlHh4bcTQoGt7-FXaCo68WA&usqp=CAU"
             title="Rental unit in Nairobi"
-            description="Unique activities we can do together, led by a world of hosts."
+            description="Convinient location with everyday essentials"
             />
             
             
@@ -43,19 +43,19 @@ function Home() {
         <Card 
              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJbzzyAwOm4SeZARzUeUPkJk0QU8W08js2Ng&usqp=CAU"
             title="Rental unit in Lavington"
-            description="Unique activities we can do together, led by a world of hosts."
+            description="A space thats all yours, with well furnished rooms "
             viewImagesUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUlhuKexoWBekJRxsVR4QHkrWJnCpqrw9iGXmJ_y48fis3h8s6Y24x_OTaba2hfGkQ8T0&usqp=CAU"
             />
             <Card 
              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkOX6FFVED8p_DNhbfM7-zRz8x974lhpSumJg_EZRr1BZGcrAkoJ1aEEb_TKTFgIFRpcE&usqp=CAU"
             title="condo in Kilimani"
-            description="Unique activities we can do together, led by a world of hosts."
+            description="Comfortable private places, stunning furnished rooms"
             viewImagesUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUlhuKexoWBekJRxsVR4QHkrWJnCpqrw9iGXmJ_y48fis3h8s6Y24x_OTaba2hfGkQ8T0&usqp=CAU"
             />
             <Card 
              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUlhuKexoWBekJRxsVR4QHkrWJnCpqrw9iGXmJ_y48fis3h8s6Y24x_OTaba2hfGkQ8T0&usqp=CAU"
             title="Condo in South B"
-            description="Unique activities we can do together, led by a world of hosts."
+            description="Apartment with luxurios furnishing. Its private and spacious"
             viewImagesUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUlhuKexoWBekJRxsVR4QHkrWJnCpqrw9iGXmJ_y48fis3h8s6Y24x_OTaba2hfGkQ8T0&usqp=CAU"
             />
         </div>
