34c4e8e33c77f2b1ab7db32b3976f65868d329e2
pendoagnes222 <pendoagnes22@gmail.com>
2023-03-24 10:58:58 +0300

    airbnb

--- client/src/components/Home/LoginPage.js
+++ client/src/components/Home/LoginPage.js
@@ -1,48 +1,17 @@
-import React, {useState} from 'react'
-import './LoginPage.css'
-import { Link } from 'react-router-dom'
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+
 import './LoginPage.css';
+
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { Alert } from 'react-bootstrap';
 export default function LoginPage() {
 
-    const { register,  handleSubmit, reset, formState: { errors } } = useForm();
+    const { register, watch, handleSubmit, reset, formState: { errors } } = useForm();
     //const onSubmit = data => console.log(data);
     const [show, setShow] = useState(false)
     const [serverResponse, setServerResponse] = useState('')
     
-  /*return (
-    <section>
-        <div className="register">
-            <div className="col-1">
-                <h2>Sign Up</h2>
-               
-                <small>Register to enjoy our services</small>
-
-                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                    <input type="text" {...register("username",{ required:true })} placeholder='username' 
-                        
-                    />
-
-                    <input type="text" {...register("mobile", { required : true, maxLength: 10 })} placeholder='mobile number'  />
-                    {errors.mobile?.type === "required" && "Mobile Number is required"}
-                    {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
-                
-
-                    <input type="email" {...register("email", { required : true, maxLength: 10 })} placeholder='email' 
-                        
-                    />
-
-                    <input type="password" {...register("password")} placeholder='password' 
-                        
-                    />
-                    <input type="password" {...register("confirmpwd")} placeholder='confirm password' 
-                        
-                    />
-                    
-                    <button className='btn'>Sign Up</button>
-                    <small>Already Registered? <Link to="/SignIn">Sign In</Link></small>
-                </form>*/
     const onSubmit = (data) => {
 
         if (data.password === data.confirmpwd) {
@@ -119,11 +88,13 @@ export default function LoginPage() {
                         {errors.confirmpwd && <span style={{ color: "red" }}> <small>Confirm password is required</small></span>}
 
                         <button className='btn' onClick={handleSubmit(onSubmit)} >Sign Up</button>
-                        <div><small>Already Registered? <Link to="/login">Sign in here</Link></small></div>
+                        <div><small>Already Registered? <Link to="/SignIn">Sign in here</Link></small></div>
                     </form>
 
                 </div>
             </div>
         </section>
     )
-                }
+}
+
+                    
