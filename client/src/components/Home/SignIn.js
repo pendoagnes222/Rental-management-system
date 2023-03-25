a2221e72ba3a40e3745459e2b195d857b640c390
jobootwori <30703963+jobootwori@users.noreply.github.com>
2023-03-25 07:01:41 +0300

    SIgn in update

--- client/src/components/Home/SignIn.js
+++ client/src/components/Home/SignIn.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import './SignIn.css'
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom'
 
 export default function SignIn() {
 
-    const { register, handleSubmit,  formState: { errors } } = useForm()
-    const onSubmit = data => console.log(data);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm()
+    const onSubmit = (data) => {
+        console.log(data)
+
+        const requestOptions={
+           method:"POST",
+           headers:{
+               'content-type':'application/json'
+           },
+           body:JSON.stringify(data)
+       }
 
     // console.log(watch('username'));
-    
+
+    fetch('/auth/login',requestOptions)
+       .then(res=>res.json())
+       .then(data=>{
+           console.log(data)
+           
+           
+       })    
+       reset()
+    }
   return (
     <section>
         <div className="register">
@@ -19,16 +38,19 @@ export default function SignIn() {
                 <form id='form' className='flex flex-col' >
                     <input type="text" {...register("username", { required: true, maxLength: 25 })} placeholder='username' />
                     {errors.username && <span style={{ color: "red" }}> <small>Username is required</small></span>}
-                    <input type="text" {...register("password")} placeholder='password' />
+                    <input type="text" {...register("password", { required: true, minLength: 8 })} placeholder='password' />
                     {errors.password && <span style={{ color: "red" }}> <small>Password is required</small></span>}
                     {errors.password?.type === "maxLength" && <p style={{color:'red'}}>
                     <small>Password should be more than 8 characters</small>
                     </p>}
-                    <button className='btn' onSubmit={handleSubmit(onSubmit)}>Sign In</button>
+                    <button className='btn' onClick={handleSubmit(onSubmit)}>Sign In</button>
+                    
+                    <small>Do not have an account? <Link to='/LogInPage'>Create One</Link></small>
+              
                 </form>
 
             </div>
         </div>
     </section>
   )
-}
+  }
