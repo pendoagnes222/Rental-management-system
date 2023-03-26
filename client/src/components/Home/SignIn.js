01ff37efa41bf5351e2c396c0e92c1f70a379168
jobootwori <30703963+jobootwori@users.noreply.github.com>
2023-03-26 16:11:04 +0300

    [Update]

--- client/src/components/Home/SignIn.js
+++ client/src/components/Home/SignIn.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './SignIn.css'
-import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { Login } from '../auth'
+import {useHistory} from 'react-router-dom'
+
 
 export default function SignIn() {
 
-    const { register, handleSubmit, reset, formState: { errors } } = useForm()
+    const { register, handleSubmit, login, reset, formState: { errors } } = useForm()
+
+    const history=useHistory()
+        
     const onSubmit = (data) => {
         console.log(data)
 
@@ -22,12 +28,13 @@ export default function SignIn() {
     fetch('/auth/login',requestOptions)
        .then(res=>res.json())
        .then(data=>{
-           console.log(data)
+           console.log(data.access_token)
+           login(data.access_token)
        .catch(err => console.log(err))   
            
-       })    
+           
        reset()
-    }
+})}
   return (
     <section>
         <div className="register">
@@ -38,11 +45,16 @@ export default function SignIn() {
                 <form id='form' className='flex flex-col' >
                     <input type="text" {...register("username", { required: true, maxLength: 25 })} placeholder='username' />
                     {errors.username && <span style={{ color: "red" }}> <small>Username is required</small></span>}
+                    {errors.username?.type === "maxLength" && <p style={{color:'red'}}>
+                    <small>Username should be less than 25 characters</small>
+                    </p>}
+
                     <input type="text" {...register("password", { required: true, minLength: 8 })} placeholder='password' />
                     {errors.password && <span style={{ color: "red" }}> <small>Password is required</small></span>}
-                    {errors.password?.type === "maxLength" && <p style={{color:'red'}}>
+                    {errors.password?.type === "minLength" && <p style={{color:'red'}}>
                     <small>Password should be more than 8 characters</small>
                     </p>}
+
                     <button className='btn' onClick={handleSubmit(onSubmit)}>Sign In</button>
                     
                     <small>Do not have an account? <Link to='/LogInPage'>Create One</Link></small>
