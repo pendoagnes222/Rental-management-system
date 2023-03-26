9871ceea2f33fe613faa71075ce7b351fa3e23e9
pendoagnes222 <pendoagnes22@gmail.com>
2023-03-27 02:49:46 +0300

    airbnb

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,51 +1,45 @@
 import React from 'react'
 import "./Footer.css"
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram, FaTelegram, } from "react-icons/fa";
+import { GrMail} from "react-icons/gr";
+import { BsFillTelephoneInboundFill} from "react-icons/bs";
 
 
 
 function Footer() {
   return (
     <div className='footer'>
-            <h1>support</h1>
-            <h2>Help center</h2>
-            <h2>Aircover</h2>
-            <h2>Supporting people with dissabilities</h2>
-            <h2>Cancellation options</h2>
-            <h2>Report a neighbour concerns</h2>
-      <div className='footer__info'>
-            <h1>Community</h1>
-            <h2>Airbnb.org:disaster relief housing</h2>
-            <h2>Combating discrimination</h2>
-        </div>  
-        <div className='footer__info2'>
-            <h1>Hosting</h1>
-            <h2>Airbnb your home</h2>
-            <h2>Aircover for Hosts</h2>
-            <h2>Explore hosting resources</h2>
-            <h2>Visiting our community forum</h2>
-            <h2>How to host responsibly</h2>
-            <h2>Airbnb-friendly apartments</h2> 
-        </div>
-        <div className='footer__info3'>
-            <h1>Airbnb</h1>
-            <h2>Newsroom</h2>
-            <h2>learn about new features</h2>
-            <h2>letter from our founders</h2>
-            <h2>Careers </h2>
-            <h2>Investors</h2>
-            <h2>Gift cards </h2> 
-        </div>
-        <hr />
-        <div className="footer__terms">
-        <h2>Copyright &copy; 2023 Airbnb,Inc. All Rights Reserved</h2>
-        </div>
-        <div className='footer__social'>
+            <section className="footer-info">
+              
+       <h2>We are based in Exmak, Nairobi </h2>
+       <p>we work with clients from all over. Get in touch with us</p>
+       <div className="contact_info">
+        <h4><GrMail className='icon' /> info@rentals.co</h4>
+        <h4><BsFillTelephoneInboundFill className='icon2'/> (254) 3345 7689</h4>
+       </div>
+       <div className='footer__social'>
         <a  className='facebook' href='https://facebook.com'>< FaFacebook/> </a>
         <a  className='twitter' href='https://Twitter.com'>< FaTwitter/> </a>
         <a  className='instagram' href='https://Instagran.com'>< FaInstagram/> </a>
+        <a  className='instagram' href='https://Instagran.com'>< FaTelegram/> </a>
+        
         
         </div>
+        <hr />
+        
+       
+
+      </section>
+      
+     {/* <section className="footer-about">
+        <p>Simplicity carried to an extreme becomes elegance <br /> design with smile</p></section>
+<section className='footer-social-media d-flex justify-content-evenly'></section>*/}
+            
+        
+        <div className="footer__terms">
+        <h2>Copyright &copy; 2023 Airbnb,Inc. All Rights Reserved</h2>
+        </div>
+        
 
     </div>
      
--- client/src/components/auth.js
+++ client/src/components/auth.js
@@ -1,15 +1,15 @@
-import { createAuthProvider } from 'react-token-auth'
+//import { createAuthProvider } from 'react-token-auth'
 
-const authProvider = createAuthProvider({
-  accessTokenKey: 'access_token',
-  onUpdateToken: (token) =>
-    fetch('/auth/refresh', {
-      method: 'POST',
-      body: token.refresh_token
-    }).then(r => r.json())
-})
+//const authProvider = createAuthProvider({
+ // accessTokenKey: 'access_token',
+ // onUpdateToken: (token) =>
+   // fetch('/auth/refresh', {
+   //   method: 'POST',
+     // body: token.refresh_token
+   // }).then(r => r.json())
+//})
 
-export const useAuth = authProvider.useAuth
-export const authFetch = authProvider.authFetch
-export const login = authProvider.login
-export const logout = authProvider.logout
+//export const useAuth = authProvider.useAuth
+//export const authFetch = authProvider.authFetch
+//export const login = authProvider.login
+//export const logout = authProvider.logout
