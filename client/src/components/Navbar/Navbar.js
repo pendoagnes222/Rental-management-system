edc11a2b4a97fce94a003d2a30846fc4f6f9da30
jobootwori <30703963+jobootwori@users.noreply.github.com>
2023-03-28 11:55:52 +0300

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,60 +1,111 @@
 import React from 'react'
 import './Navbar.css'
-import { FaUserCircle, FaGlobe,FaSearch} from 'react-icons/fa'
-import img from '../Images/Airbnb-logo.jpg' 
+import { FaUserCircle, FaGlobe, FaSearch } from 'react-icons/fa'
+import img from '../Images/Airbnb-logo.jpg'
+import { useAuth } from '../auth'
 
 /*import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";*/
-import { Link, useNavigate} from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+
+
+
+const LoggedInLink = () => {
+  const navigate = useNavigate();
+  return (
+    
+    <div className='header__right'>
+    <h1>apartments</h1>
+
+    <FaGlobe className='language' />
+    {/*<button onClick={() =>{
+              navigate("/loginpage")
+            }}> Post  apartment</button>*/}
+
+    <FaUserCircle className='user__icon'
+      onClick={() => {
+        navigate("/");
+      }} />
+
+  </div>
+  )
+}
+
+
+const LoggedOutLinks = () => {
+
+  const navigate = useNavigate();
+
+  return (
+    
+    <div className='header__right'>
+    <h1>apartments</h1>
+
+    <FaGlobe className='language' />
+    {/*<button onClick={() =>{
+              navigate("/loginpage")
+            }}> Post  apartment</button>*/}
+
+    <FaUserCircle className='user__icon'
+      onClick={() => {
+        navigate("/loginpage");
+      }} />
+
+  </div>
+  )
+}
+
+
 function Navbar() {
-  
-  let navigate = useNavigate();
+
+  const navigate = useNavigate();
+  const [logged] = useAuth();
   return (
     <div className='header'>
-        <Link to="/">
+      <Link to="/">
         <img
-            className="header__icon"
+          className="header__icon"
 
-            src={img}
-           // src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-            alt="BandB Solutions"
-        
+          src={img}
+          // src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt="BandB Solutions"
+
         />
-        </Link>
-        <div className='header__center'>
+      </Link>
+      <div className='header__center'>
         <button onClick={() => navigate('/searchpage')}
-             variant='outlined'>Search<FaSearch className='icon'/>
-            </button>
-                {/*<input type="text" />
+          variant='outlined'>Search<FaSearch className='icon' />
+        </button>
+        {/*<input type="text" />
                    <FaSearch/>*/}
-         </div>
+      </div>
 
-         <div className='header__right'>
-          <h1>apartments</h1>
+      <div className='header__right'>
+        <h1>apartments</h1>
 
-                <FaGlobe className='language'/>
-                {/*<button onClick={() =>{
+        <FaGlobe className='language' />
+        {/*<button onClick={() =>{
                   navigate("/loginpage")
                 }}> Post  apartment</button>*/}
-              
-                <FaUserCircle  className='user__icon'
-               onClick={() =>{
-            navigate("/loginpage");
-           }} />
-           
-                
-            
-                
-            </div>
-        
 
-   
-    
-</div>
-)
+        <FaUserCircle className='user__icon'
+          onClick={() => {
+            navigate("/loginpage");
+          }} />
+
+
+
+
+      </div>
+
+
+
+
+    </div>
+  )
 }
 
 export default Navbar;
